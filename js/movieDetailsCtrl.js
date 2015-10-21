9acb22c19efb4d54cc345dc44a7bb3a87b12ec73
Barney <btong21@gmail.com>
2015-10-20 23:24:14 -0400

    v2

--- js/movieDetailsCtrl.js
+++ js/movieDetailsCtrl.js
@@ -1,7 +1,15 @@
 app.controller('MovieDetailsCtrl', MovieDetailsCtrl);
 
-function MovieDetailsCtrl(movieService) {
+function MovieDetailsCtrl(movieService, $routeParams) {
   /*TODO #4: Load the movie from the movieService using the id form the route params*/
-  this.movieID = $routeParams.movieID;
+ 
+  this.movieService = movieService;
+  this.routeParams = $routeParams;
+  this.movieID = this.routeParams.movieID;
+  this.movie = this.movieService.getMovie(this.movieID);
 }
+
+
+
+
 
--- js/movieListCtrl.js
+++ js/movieListCtrl.js
@@ -1,15 +1,27 @@
 app.controller('MovieListCtrl', MovieListCtrl);
 
-function MovieListCtrl(movieService) {
+function MovieListCtrl(movieService, $location) {
   /*TODO #3: Load all of the movies from the movieService*/
   this.movies = movieService.getMovies();
-  console.log(this.movies);
+  this.location = $location;
+  // this.movieService = movieService
   
   this.sortOptions = [
     {label: 'Title', sortField: 'Title', reverse: false},
     {label: 'Rating', sortField: "imdbRating", reverse: true}
   ]
   
+  this.selectedOption = this.sortOptions[0];
+
   this.curPage = 0;
   this.moviesPerPage = 4;
+}
+
+MovieListCtrl.prototype.ChangeID = function(id) {
+	this.location.path('/movie/' + id);
+
+}
+
+MovieListCtrl.prototype.getNumPages = function() {
+	return Math.ceil(this.movies.length/this.moviesPerPAge);
 }
